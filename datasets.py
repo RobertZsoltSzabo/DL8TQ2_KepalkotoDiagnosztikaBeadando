36ccdc65afca77fe27333e8765a7f17f2bfc1c36
Robert Zsolt Szabo <szabo.rozso@gmail.com>
2023-07-14 15:38:22 -0400

    cleanup

--- datasets.py
+++ datasets.py
@@ -5,6 +5,14 @@ from PIL import Image
     
 
 class GrayscaleImageFolder(Dataset):
+    '''
+    Custom dataset based on the concept of the built-in ImageFolder dataset. Expects single channel (grayscale) inputs.
+    args:
+        - root: root folder where the images are stored
+        - filename_filter: only files that have the provided string in their title will be fetched
+        - image_extension: file extension of images
+        - transform: transforms applied to returned data at fetch time
+    '''
     def __init__(self, root, filename_filter='', image_extension='png', transform=None):
         self.transform = transform
         self.image_files = [file for file in list(Path(root).glob(f'**/*.{image_extension}')) if filename_filter in str(file)]
